4faec63696296377ea7faf30379482e2d5319eb7
Sumeruk <zinchenkokostantin@yandex.ru>
2024-04-26 13:51:06 +0300

    make connection with BD with JDBC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import org.example.BD.JDBC;
+import org.example.BD.JDBCImpl;
 import org.example.cli.Menu;
 
 public class Main {
     public static void main(String[] args) {
+
+        JDBC jdbc = JDBCImpl.getInstance();
+        jdbc.connection();
         Menu.run();
+        jdbc.closeConnection();
     }
 }

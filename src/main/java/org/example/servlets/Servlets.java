c07ca9836a3a45f88eee54ff98d2fc00d1c8abf3
Sumeruk <zinchenkokostantin@yandex.ru>
2024-05-14 20:32:46 +0300

    дальше делают мужики с завода

--- src/main/java/org/example/servlets/Servlets.java
+++ src/main/java/org/example/servlets/Servlets.java
@@ -1,6 +1,5 @@
 package org.example.servlets;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,13 +8,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-// @WebServlet("/")
+@WebServlet("/myServlet")
 public class Servlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.print("<h1>Hello, world!</h1>");
+        writer.close();
     }
 
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 12
+  Date: 14.05.2024
+  Time: 16:54
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+index.jsp
+</body>
+</html>

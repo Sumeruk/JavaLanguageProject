9715eae1f2e543936870f0eca6696f49a5667c39
Sumeruk <zinchenkokostantin@yandex.ru>
2024-05-15 14:39:15 +0300

    add response with json

--- src/main/java/org/example/servlets/Servlets.java
+++ src/main/java/org/example/servlets/Servlets.java
@@ -4,18 +4,40 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/myServlet")
 public class Servlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
-        resp.setContentType("text/html");
+        resp.setContentType("application/json");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws  IOException{
+        resp.setContentType("application/json");
         PrintWriter writer = resp.getWriter();
-        writer.print("<h1>Hello, world!</h1>");
-        writer.close();
+
+        String name = req.getParameter("username");
+        String age = req.getParameter("userage");
+        String gender = req.getParameter("gender");
+        String country = req.getParameter("country");
+        String[] courses = req.getParameterValues("courses");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", name);
+        jsonObject.put("userage", age);
+        jsonObject.put("gender", gender);
+        jsonObject.put("country", country);
+        jsonObject.put("courses", courses);
+
+        String jsonResponse = jsonObject.toString();
+        resp.getWriter().write(jsonResponse);
+
+
     }
 
 }
